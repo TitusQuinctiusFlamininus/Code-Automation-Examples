0482f3727f563771d526a766af91cc79702ae3d6
Michael Nyika <michael.nyika@unity3d.com>
2024-06-24 19:18:36 +0200

    all tests now enabled

--- datarade-code-challenge/src/test/java/datarade/OWASPShop_TestSuite.java
+++ datarade-code-challenge/src/test/java/datarade/OWASPShop_TestSuite.java
@@ -4,10 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({//OWASPShop_ItemPaginationTests.class,
-        //OWASPShop_ItemPerPageTests.class,
-        //           OWASPShop_LanguageTests.class,
-        //           OWASPShop_LoginAuthenticationTests.class,
+@Suite.SuiteClasses({OWASPShop_ItemPaginationTests.class,
+                     OWASPShop_ItemPerPageTests.class,
+                     OWASPShop_LanguageTests.class,
+                     OWASPShop_LoginAuthenticationTests.class,
                      OWASPShop_SecurityFlawTests.class
                     })
 public class OWASPShop_TestSuite {
