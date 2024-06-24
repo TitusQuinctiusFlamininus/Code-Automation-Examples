7a76ef6a0265f2ea7fbc341222026c19e5f4eb52
Michael Nyika <michael.nyika@unity3d.com>
2024-06-24 16:19:02 +0200

    authentication tests complete

--- datarade-code-challenge/src/test/java/datarade/OWASPShop_Enums.java
+++ datarade-code-challenge/src/test/java/datarade/OWASPShop_Enums.java
@@ -9,7 +9,17 @@ public class OWASPShop_Enums {
         EmailTextFieldPath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-login/div/mat-card/div/mat-form-field[1]/div/div[1]/div[3]/input"),
         PasswordTextFieldPath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-login/div/mat-card/div/mat-form-field[2]/div/div[1]/div[3]/input"),
         LoginPageButtonPath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-login/div/mat-card/div/button[1]"),
-        AuthAttemptMessagePath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-login/div/mat-card/div[1]");
+        AuthAttemptMessagePath("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-login/div/mat-card/div[1]"),
+        ValidUsername("admin@juice-sh.op"),
+        ValidPassword("admin123"),
+        LogoutButtonPath("/html/body/div[4]/div[2]/div/div/div/button[4]"),
+        SuccessLoginMessage_1_Path("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-challenge-solved-notification/div/mat-card[1]/div[1]"),
+        SuccessLoginMessage_2_Path("/html/body/app-root/div/mat-sidenav-container/mat-sidenav-content/app-challenge-solved-notification/div/mat-card[2]/div[1]"),
+        SuccessLoginMessage_1("You successfully solved a challenge: Password Strength (Log in with the administrator's user credentials without previously changing them or applying SQL Injection.)\n" +
+                "X"),
+        SuccessLoginMessage_2("You successfully solved a challenge: Login Admin (Log in with the administrator's user account.)\n" +
+                "X");
+        ;
 
         public final String label;
         private Authentication(String label) {
