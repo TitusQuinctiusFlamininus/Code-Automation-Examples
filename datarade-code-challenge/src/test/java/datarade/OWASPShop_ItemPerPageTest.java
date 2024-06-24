2268f762fc75fdd5ab311937bb2a7705dccc2b6b
Michael Nyika <michael.nyika@unity3d.com>
2024-06-24 09:49:45 +0200

    items per page tests all complete

--- datarade-code-challenge/src/test/java/datarade/OWASPShop_ItemPerPageTest.java
+++ datarade-code-challenge/src/test/java/datarade/OWASPShop_ItemPerPageTest.java
@@ -39,15 +39,29 @@ public class OWASPShop_ItemPerPageTest extends OWASPSHOP_JuicyTestSuite{
         driver.quit();
     }
 
+    /*
+   Test to check that the Option for 12 items per page means that the first page has 12 items,
+   the second page also has 12 and the last page has 11 items
+  */
     @Ignore
     @Test
-    public void verify_Lowest_Items_Per_Page_Count()
+    public void verify_Lowest_Items_Per_Page_Count_Correct_On_Every_Page()
     {
         try {
             Thread.sleep(3000);
-            List<WebElement> allPageItems = getAllWebElements("Printing All First Page Items ...\n");
+            List<WebElement> firstPageItems = getAllWebElements("Printing First Page Items ...\n");
             Thread.sleep(3000);
-            assertEquals("There are meant to be 12 Items on the Landing Page", 12, allPageItems.size());
+            clickNextPageButton(); //Go to the second page
+            Thread.sleep(3000);
+            List<WebElement> secondPageItems = getAllWebElements("Printing Second Page Items ...\n");
+            Thread.sleep(3000);
+            clickNextPageButton(); //Go to the last page
+            Thread.sleep(3000);
+            List<WebElement> lastPageItems = getAllWebElements("Printing Last Page Items ...\n");
+            Thread.sleep(3000);
+            assertEquals("There are meant to be 12 Items on the Landing Page", 12, firstPageItems.size());
+            assertEquals("There are meant to be 12 Items on the Second Page", 12, secondPageItems.size());
+            assertEquals("There are meant to be 12 Items on the Last Page", 11, lastPageItems.size());
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -62,20 +76,47 @@ public class OWASPShop_ItemPerPageTest extends OWASPSHOP_JuicyTestSuite{
     Test to check that the Option for 24 items per page means that the first page has 24 items
     and the last page has 11, making a total of 35 Store items
    */
+    @Ignore
     @Test
-    public void verify_Average_Number_Of_Items_Per_Page_Count()
+    public void verify_Average_Number_Of_Items_Per_Page_Count_Correct_Count_On_Every_Page()
     {
         try {
             Thread.sleep(3000);
             selectFromDropdown("24");
             Thread.sleep(3000);
             List<WebElement> allFirstPageItems = getAllWebElements("Printing All First Page Items ...\n");
-            Thread.sleep(3000); //Go to the last page
-            clickNextPageButton();
+            Thread.sleep(3000);
+            clickNextPageButton(); //Go to the last page
             Thread.sleep(3000);
             List<WebElement> allLastPageItems = getAllWebElements("Printing All Last Page Items ...\n");
             assertEquals("There are meant to be 24 Items on the Landing Page", 24, allFirstPageItems.size());
             assertEquals("There are meant to be 12 Items on the Last Page", 11, allLastPageItems.size());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            driver.manage().deleteAllCookies();
+            cleanUp();
+        }
+    }
+
+    /*
+  Test to check that the Option for 24 items per page means that the first page has 24 items
+  and the last page has 11, making a total of 35 Store items
+ */
+
+    @Ignore
+    @Test
+    public void verify_Highest_Number_Of_Items_Per_Page_Count_Correct_Count_On_Every_Page()
+    {
+        try {
+            Thread.sleep(3000);
+            selectFromDropdown("36");
+            Thread.sleep(3000);
+            List<WebElement> allPageItems = getAllWebElements("Printing All Landing Page Items ...\n");
+            Thread.sleep(3000);
+            assertEquals("There are meant to be 35 Items on the Landing Page", 35, allPageItems.size());
         }
         catch (Exception e) {
             e.printStackTrace();
