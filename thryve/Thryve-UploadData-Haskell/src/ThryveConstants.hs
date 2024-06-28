6e8e7ae35b61048c6809813ce75248bd2b473041
TitusQuinctiusFlamininus <25204175+TitusQuinctiusFlamininus@users.noreply.github.com>
2024-06-28 09:43:26 +0200

    Update ThryveConstants.hs

--- thryve/Thryve-UploadData-Haskell/src/ThryveConstants.hs
+++ thryve/Thryve-UploadData-Haskell/src/ThryveConstants.hs
@@ -9,13 +9,13 @@ import qualified Data.Map as M
 
 -- A Map of seemingly constant values
 thryveConstants :: ThryveConstants
-thryveConstants   =    M.fromList [("userCreationUrl","https://qa.und-gesund.de/restjson/v5/accessToken"),
-                                          ("uploadDataUrl","https://qa.und-gesund.de/restjson/v5/userInformation"),
-                                          ("downloadDataUrl","https://qa.und-gesund.de/restjson/v5/dynamicEpochValues"),
-                                          ("username","applicationtest-api"),
-                                          ("password","fyNbjcr5g9GUS5tE"),
-                                          ("authID","mmixU2uX6Q31mrIg"),
-                                          ("authSecret","5y81Oeht3YrO2ooQkCJVuqiXcVtbLmIVzXyBh21HSePqBCxL8riuYAp8pD3jQ1DW"),
+thryveConstants   =    M.fromList [("userCreationUrl","someUserCreationUrl"),
+                                          ("uploadDataUrl","someDataUploadUrl"),
+                                          ("downloadDataUrl","someDataDownloadUrl"),
+                                          ("username","someUsername"),
+                                          ("password","somePassword"),
+                                          ("authID","someAuthID"),
+                                          ("authSecret","someAuthSecrezt"),
                                           ("testHeight","167"),
                                           ("testWeight","75.6")
                                   ]
